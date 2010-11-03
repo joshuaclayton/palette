43c964ef1147621c9206d337a17aaeb998e0d543
Joshua Clayton <jclayton@thoughtbot.com>
2010-11-02 22:31:36 -0400

    Clean up some syntax

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,8 @@ require "aruba"
 
 Before do
   schemes_origin_dir = File.join(%w(features fixtures schemes))
-  schemes_dir = File.join(dirs.first, schemes_origin_dir)
+  schemes_dir        = File.join(dirs.first, schemes_origin_dir)
+
   FileUtils.mkdir_p(schemes_dir)
   FileUtils.cp(Dir.glob(File.join(schemes_origin_dir, "*")), schemes_dir)
 end
